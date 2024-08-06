b4d4d1b2ecca05cea53f5e44323fe1f36973b41e
skdud5126 <skdud5126@gmail.com>
2024-08-06 11:11:46 +0900

    upload0 24.08.06

---
+++ algo/stack/stack1/stack_my_stack.py
@@ -0,0 +1,22 @@
+stack_size = 10
+
+stack = [0] * stack_size
+top = -1
+
+# push 1, 2, 3
+
+top += 1   # push(1)
+stack[top] = 1
+
+top += 1
+stack[top] = 2
+
+top += 1
+stack[top] = 3
+
+top -= 1  # pop()
+print(stack[top+1])
+print(stack[top])
+top -= 1
+print(stack[top])
+top -= 1
