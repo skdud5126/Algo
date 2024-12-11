02644dfd80ec95589246ebae21db6e30fe83c9cb
skdud5126 <skdud5126@gmail.com>
2024-12-11 15:18:22 +0900

    up1 24.12.11

--- algo/BAEKJOON/2480.[주사위 세개]/2480.py
+++ algo/BAEKJOON/2480.[주사위 세개]/2480.py
@@ -21,4 +21,14 @@
 
 a,b,c = map(int, input().split())
 
-print(a, b, c)
+if a==b & b==c:  # 세 개의 눈이 같은 경우
+    print(10000+(a*1000))
+elif a==b or a==c:  # 두 개의 눈이 같은 경우
+    print(1000+(a*100))
+elif b==c:
+    print(1000+(b*100))
+else:
+    print(max(a,b,c)*100)
+
+
+
